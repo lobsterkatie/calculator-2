665e692d31154f78400e37b55d8de144aa760ed6
Hackbright Student <info@hackbrightacademy.com>
2015-09-23 16:58:50 -0700

    first commit, calc working

---
+++ arithmetic.py
@@ -0,0 +1,48 @@
+def add(arg_list):
+    num1 = int(arg_list[0])
+    num2 = int(arg_list[1])
+
+    return num1 + num2
+
+def subtract(arg_list):
+    num1 = int(arg_list[0])
+    num2 = int(arg_list[1])
+
+    return num1 - num2
+
+def multiply(arg_list):
+    num1 = int(arg_list[0])
+    num2 = int(arg_list[1])
+
+    return num1 * num2
+
+def divide(arg_list):
+    num1 = int(arg_list[0])
+    num2 = int(arg_list[1])
+
+    return float(num1)/num2
+
+def square(arg_list):
+    num1 = int(arg_list[0])
+
+    return num1 ** 2
+
+def cube(arg_list):
+    num1 = int(arg_list[0])
+
+    return num1 ** 3
+
+def power(arg_list):
+    base = int(arg_list[0])
+    exponent = int(arg_list[1])
+
+    return base ** exponent
+
+def mod(arg_list):
+    num1 = int(arg_list[0])
+    num2 = int(arg_list[1])
+
+    return num1 % num2
+
+
+
