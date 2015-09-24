ac68bac4313f0f83213eced25427256b3d8181c7
Hackbright Student <info@hackbrightacademy.com>
2015-09-23 18:02:22 -0700

    UNTESTED - examples of lots of stuff

--- arithmetic.py
+++ arithmetic.py
@@ -1,14 +1,14 @@
 def add(arg_list):
     sum = 0
-    for index in range(0,len(arg_list)):
-        addend = int(arg_list[index])
+    for num in arg_list:
+        addend = int(num)
         sum += addend
 
     return sum
 
 def subtract(arg_list):
     difference = 0
-    for index in range(0,len(arg_list)):
+    for index, num in enumerate(arg_list):
         subtractend = int(arg_list[index])
         difference -= subtractend
 
@@ -23,8 +23,8 @@ def multiply(arg_list):
     return product
 
 def divide(arg_list):
-    quotient = float(arg_list[0])
-    for index in range(1,len(arg_list)):
+    quotient = arg_list[0]
+    for num in arg_list[1:]:
         divisor = float(arg_list[index])
         quotient /= divisor
         print "q= " , quotient
@@ -42,9 +42,9 @@ def cube(arg_list):
     return num1 ** 3
 
 def power(arg_list):
-    result = int(arg_list[0])
-    for index in range(1,len(arg_list)):
-        exponent = float(arg_list[index])
+    result = arg_list.pop(1)
+    for num in arg_list:
+        exponent = float(num)
         result = result ** exponent
 
     return result
